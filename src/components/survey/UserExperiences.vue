fe441fc0bf4c6a740c350bd40298f2743aa0a38a
Yaroslav <dombrovskiy91@gmail.com>
2020-12-01 11:48:00 +0200

    catch error from Server

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -6,6 +6,7 @@
         <base-button @click="loadExpirience">Load Submitted Experiences</base-button>
       </div>
       <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
       <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found. Start adding some survey results first</p>
 
       <ul v-else-if="!isLoading && results && results.length > 0">
@@ -32,11 +33,13 @@ export default {
     return {
       results: [],
       isLoading: false,
+      error: null,
     };
   },
   methods: {
     loadExpirience() {
     this.isLoading = true;
+    this.error = null;
     //GET req default
     fetch('https://vue-http-demo-48c31.firebaseio.com/surveys.json')
         .then((responce) =>{
@@ -55,6 +58,11 @@ export default {
             });
           }
           this.results = results;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+          this.error = "Failed to fetch data, please try again later.."
         });
     },
   },
